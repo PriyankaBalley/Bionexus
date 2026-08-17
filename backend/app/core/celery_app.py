a4161cb67d55bd7a4354856ebadc87657f949f36
Priyanka Balley <priyankaballey120@gmail.com>
2026-08-17 16:26:58 +0530

    Update backend

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -3,7 +3,7 @@ from celery import Celery
 from .config import settings
 
 celery_app = Celery(
-    "editease",
+    "bionexus",
     broker=settings.CELERY_BROKER_URL,
     backend=settings.CELERY_RESULT_BACKEND,
     include=[
